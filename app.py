1ed64d951e23c02c1ff998349de0997b3372a996
joshirajendra1 <rajendrajoshi@Rajendras-MacBook-Pro.local>
2023-03-29 08:11:41 -0700

    added config file

--- app.py
+++ app.py
@@ -1,23 +1,12 @@
 """
 Your module description
 """
-import os
-import ast
-import torch
-import pickle
 import json
-import pandas as pd
-import numpy as np
-from torch import nn
-from sklearn.preprocessing import OneHotEncoder
-from sklearn.metrics import mean_absolute_error
-from torch.utils.data import Dataset, DataLoader
 
 # import model and data module for ML
-from src.regression import Regression
-from src.input_data import InputData
 
 from model import PredictScore
+
 
 def lambda_handler(event, context):
     """Sample pure Lambda function
@@ -29,11 +18,6 @@ def lambda_handler(event, context):
     return {
         "statusCode": 200,
         "body": json.dumps(
-            {
-                "message": "Your predicted output is " + str(predicted_score),
-            }
+            {"message": "Your predicted output is " + str(predicted_score),}
         ),
     }
-
-
-
