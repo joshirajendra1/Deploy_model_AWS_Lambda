8832f2056357aaf15b06d71572c289637b8bbde8
joshirajendra1 <joshirajendra1@users.noreply.github.com>
2023-03-29 14:51:06 +0000

    :art: Format Python code with psf/black

--- src/regression.py
+++ src/regression.py
@@ -10,17 +10,14 @@ from torch.utils.data import Dataset, DataLoader
 class Regression(nn.Module):
     def __init__(self, num_features):
         super(Regression, self).__init__()
-        
+
         self.layer_1 = nn.Linear(num_features, 16)
         self.layer_2 = nn.Linear(16, 10)
         self.out = nn.Linear(10, 1)
         self.relu = nn.ReLU()
 
-        
     def forward(self, inputs):
         x = self.relu(self.layer_1(inputs))
         x = self.relu(self.layer_2(x))
         x = self.out(x)
-        return (x)
-    
-
+        return x
