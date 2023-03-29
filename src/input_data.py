3e0958b51cd774b48d954d59370a36cd7e1e9070
joshirajendra1 <joshirajendra1@users.noreply.github.com>
2023-03-29 14:53:11 +0000

    :art: Format Python code with psf/black

--- src/input_data.py
+++ src/input_data.py
@@ -1,14 +1,13 @@
 from torch.utils.data import Dataset, DataLoader
 
+
 class InputData(Dataset):
-    
     def __init__(self, features, target):
         self.features = features
-        self.target   = target
-        
+        self.target = target
+
     def __getitem__(self, index):
         return self.features[index], self.target[index]
-        
-    def __len__ (self):
-        return len(self.features)
 
+    def __len__(self):
+        return len(self.features)
